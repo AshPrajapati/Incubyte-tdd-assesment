ea5f60828af677b2b9a9befc4c5d77850c470f0d
Aayush Prajapati <ashprajapati67@gmail.com>
2022-09-04 23:23:21 +0530

    new test added

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ class StringCalculator{
         if(numbers=="")return 0;
     
         if(numbers.length==1 && +numbers>=0 && +numbers<=9)return +numbers;
-    
+
+        const list_of_numbers = numbers.split(",");
+        let sum=0;
+
+        list_of_numbers.forEach(element => {
+            if(element!='')sum+=(+element);
+        });
+        return sum;
     }
 };
 
--- index.spec.js
+++ index.spec.js
@@ -10,3 +10,16 @@ test("when input string is empty",()=>{
 test("when input string is of length 1 and digit is between 0 to 9",()=>{
     expect(calcObject.add("1")).toBe(1);
 })
+
+test("when the two inputs are given",()=>{
+    expect(calcObject.add("1,2")).toBe(3);
+})
+
+test("when the string ends with comma",()=>{
+    expect(calcObject.add("1,3,")).toBe(4);
+})
+
+test("when the string contains two or more adjacent commas",()=>{
+    expect(calcObject.add("1,,3")).toBe(4);
+    expect(calcObject.add("1,,3,,,6,")).toBe(10);
+})
