f90d6526b632cfcdcb7005a74f433726c2f088f2
Aayush Prajapati <ashprajapati67@gmail.com>
2022-09-05 15:11:42 +0530

    use of assertion error

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-class StringCalculator{
-    constructor(){}
+class StringCalculator {
+  constructor() {}
 
-    /*
+  /*
         access modifier: public
         method:add
         parameter: string 
     */
-    add(numbers)
-    {
-        //when the string is empty
-        if(numbers=="")return 0;
-        
-        if(numbers.length==1 && +numbers>=0 && +numbers<=9)return +numbers;
+  add(numbers) {
+    //when the string is empty
+    if (numbers == "") return 0;
 
-        let regexpression=/[\n,]+/;
+    let regexpression = /[\n,]+/;
 
-        //Conditions for delimiter
-        if(numbers.length>=4 && numbers.substr(0,2)=="//" && numbers[3]=="\n")
-        {
-            let parameter = "[\\n"+numbers[2]+"]+"
-            regexpression = new RegExp(parameter);
-            numbers = numbers.substr(4);
-        }
-        
-        //list of numbers split by regex
-        const list_of_numbers = numbers.split(regexpression);
-        let sum=0;
-        let flag = false;
-        let negative_elements = [];
-
-        //iterating on each element in the list_of_numbers
-        list_of_numbers.forEach(element => {
-            //handling lower case alphabet
-            if(element.length==1 && element>='a' && element<='z')sum+=(element.charCodeAt(0)-97+1);
-            //handling the case when number is greater than 1000 and non empty string
-            else if(element!='' && ((+element)<=1000) && ((+element)>=0))sum+=(+element);
-            //handling case for negative elements
-            else if((+element)<0)
-            {
-                flag=true;
-                negative_elements.push(+element);
-            }
-        });
-
-        if(flag)
-        {
-            //throw error with message
-            throw new TypeError("Negatives not allowed. Negative elements are: " + negative_elements);
-        }
-        return sum;
+    //Conditions for delimiter
+    if (
+      numbers.length >= 4 &&
+      numbers.substr(0, 2) == "//" &&
+      numbers[3] == "\n"
+    ) {
+      let parameter = "[\\n" + numbers[2] + "]+";
+      regexpression = new RegExp(parameter);
+      numbers = numbers.substr(4);
     }
-};
 
-module.exports=StringCalculator;
+    //list of numbers split by regex
+    const list_of_numbers = numbers.split(regexpression);
+    let sum = 0;
+    let negative_elements = [];
+
+    //iterating on each element in the list_of_numbers
+    list_of_numbers.forEach((element) => {
+      //handling lower case alphabet
+      if (element.length == 1 && element >= "a" && element <= "z")
+        sum += element.charCodeAt(0) - 97 + 1;
+      //handling the case when number is greater than 1000 and non empty string
+      else if (element != "" && +element <= 1000 && +element >= 0)
+        sum += +element;
+      //handling case for negative elements
+      else if (+element < 0) {
+        negative_elements.push(+element);
+      }
+    });
+
+    if (negative_elements.length>=1) {
+      //throw error with message
+      throw new TypeError(
+        "Negatives not allowed. Negative elements are: " + negative_elements
+      );
+    }
+    return sum;
+  }
+}
+
+module.exports = StringCalculator;
--- index.spec.js
+++ index.spec.js
@@ -1,107 +1,104 @@
-
+const assert = require("assert");
 const StringCalculator = require("./index");
 
 const calcObject = new StringCalculator();
 
-
 /*test  1*/
-test("when input string is empty",()=>{
-    expect(calcObject.add("")).toBe(0);
-})
+test("when input string is empty", () => {
+  expect(calcObject.add("")).toBe(0);
+});
 
 /*test  2*/
-test("when input string is of length 1 and digit is between 0 to 9",()=>{
-    expect(calcObject.add("1")).toBe(1);
-})
+test("when input string is of length 1 and digit is between 0 to 9", () => {
+  expect(calcObject.add("1")).toBe(1);
+});
 
 /*test  3*/
-test("when the two inputs are given",()=>{
-    expect(calcObject.add("1,2")).toBe(3);
-})
+test("when the two inputs are given", () => {
+  expect(calcObject.add("1,2")).toBe(3);
+});
 
 /*test  4*/
-test("when the string ends with comma",()=>{
-    expect(calcObject.add("1,3,")).toBe(4);
-})
+test("when the string ends with comma", () => {
+  expect(calcObject.add("1,3,")).toBe(4);
+});
 
 /*test  5*/
-test("when the string contains two or more adjacent commas",()=>{
-    expect(calcObject.add("1,,3")).toBe(4);
-    expect(calcObject.add("1,,3,,,6,")).toBe(10);
-})
+test("when the string contains two or more adjacent commas", () => {
+  expect(calcObject.add("1,,3")).toBe(4);
+  expect(calcObject.add("1,,3,,,6,")).toBe(10);
+});
 
 /*test  6*/
-test("Numbers bigger than 1000 should be ignored",()=>{
-    expect(calcObject.add("2,1004")).toBe(2);
-    expect(calcObject.add("10000,7500")).toBe(0);
-    expect(calcObject.add("2,10,100,8,1050")).toBe(120);
-    expect(calcObject.add("10001,,7,2,1,,10002,")).toBe(10);
-})
+test("Numbers bigger than 1000 should be ignored", () => {
+  expect(calcObject.add("2,1004")).toBe(2);
+  expect(calcObject.add("10000,7500")).toBe(0);
+  expect(calcObject.add("2,10,100,8,1050")).toBe(120);
+  expect(calcObject.add("10001,,7,2,1,,10002,")).toBe(10);
+});
 
 /*test  7*/
-test("handling an unknown amount of numbers",()=>{
-    expect(calcObject.add("2,3")).toBe(5);
-    expect(calcObject.add("1,1,1,1,1,1,1,1,1,1,10,10")).toBe(30);
-    expect(calcObject.add("2,2,2,2,,,2")).toBe(10);
-    expect(calcObject.add("1002,20,30,3000")).toBe(50);
-    expect(calcObject.add("1004,1007,10067,8752")).toBe(0);
-})
+test("handling an unknown amount of numbers", () => {
+  expect(calcObject.add("2,3")).toBe(5);
+  expect(calcObject.add("1,1,1,1,1,1,1,1,1,1,10,10")).toBe(30);
+  expect(calcObject.add("2,2,2,2,,,2")).toBe(10);
+  expect(calcObject.add("1002,20,30,3000")).toBe(50);
+  expect(calcObject.add("1004,1007,10067,8752")).toBe(0);
+});
 
 /*test  8*/
-test("when string contains lower case alphabet",()=>{
-    expect(calcObject.add("a,c")).toBe(4);
-    expect(calcObject.add("1,3,a,d,10")).toBe(19);
-    expect(calcObject.add("1,20,10067,e,j,o,t,y,,,")).toBe(96);
-})
+test("when string contains lower case alphabet", () => {
+  expect(calcObject.add("a,c")).toBe(4);
+  expect(calcObject.add("1,3,a,d,10")).toBe(19);
+  expect(calcObject.add("1,20,10067,e,j,o,t,y,,,")).toBe(96);
+});
 
 /*test  9*/
-test("handle new lines between numbers",()=>{
-    expect(calcObject.add("2\n5,5")).toBe(12);
-    expect(calcObject.add("2\n2\n2\n\n,,")).toBe(6);
-})
+test("handle new lines between numbers", () => {
+  expect(calcObject.add("2\n5,5")).toBe(12);
+  expect(calcObject.add("2\n2\n2\n\n,,")).toBe(6);
+});
 
 /*test  10*/
-test("Negative elements are not allowed",()=>{
+test("Negative elements are not allowed", () => {
+  //Test case 1:
 
-    //Test case 1: 
-
-    try
+  assert.throws(
+    () => {
+      throw calcObject.add("-5,2");
+    },
     {
-        calcObject.add("-5,2");
+      message: "Negatives not allowed. Negative elements are: -5",
     }
-    catch(error)
+  );
+
+  //test case 2
+
+  assert.throws(
+    () => {
+      throw calcObject.add("-1,-2,-3,0,17,90");
+    },
     {
-        expect(error).toBeInstanceOf(TypeError);
-        expect(error).toHaveProperty('message', "Negatives not allowed. Negative elements are: -5");
+      message: "Negatives not allowed. Negative elements are: -1,-2,-3",
     }
+  );
 
-    //test case 2
-
-    try{
-        calcObject.add("-1,-2,-3,0,17,90");
-    }
-    catch(error)
-    {
-        expect(error).toBeInstanceOf(TypeError);
-        expect(error).toHaveProperty('message', "Negatives not allowed. Negative elements are: -1,-2,-3");
-    }
-})
-
+});
 
 /*test  11*/
-test("support different delimiters",()=>{
-    expect(calcObject.add("//;\n1;2")).toBe(3);
-})
+test("support different delimiters", () => {
+  expect(calcObject.add("//;\n1;2")).toBe(3);
+});
 
 /*test  12*/
-test("Negative elements with delimiter are not allowed",()=>{
-    
-    try{
-        calcObject.add("//;\n-1;-2;-3;0;17;90");
-    }
-    catch(error)
-    {
-        expect(error).toBeInstanceOf(TypeError);
-        expect(error).toHaveProperty('message', "Negatives not allowed. Negative elements are: -1,-2,-3");
-    }
-})
+test("Negative elements with delimiter are not allowed", () => {
+  try {
+    calcObject.add("//;\n-1;-2;-3;0;17;90");
+  } catch (error) {
+    expect(error).toBeInstanceOf(TypeError);
+    expect(error).toHaveProperty(
+      "message",
+      "Negatives not allowed. Negative elements are: -1,-2,-3"
+    );
+  }
+});
